1db949db9999a128ded8eeb88f51a2ba54103522
magno <magnopacheco67@gmail.com>
2023-07-28 11:51:03 -0300

    Edit NewClycleForm to include cycles data at datalist options

--- src/pages/Home/NewCycleForm/index.tsx
+++ src/pages/Home/NewCycleForm/index.tsx
@@ -4,7 +4,7 @@ import { useFormContext } from 'react-hook-form'
 import { CyclesContext } from '../../../contexts/CyclesContext'
 
 export function NewCycleForm() {
-  const { activeCycle } = useContext(CyclesContext)
+  const { activeCycle, cycles } = useContext(CyclesContext)
   const { register } = useFormContext()
   return (
     <FormContainer>
@@ -18,10 +18,9 @@ export function NewCycleForm() {
       />
 
       <datalist id="task-suggestions">
-        <option value="Projeto 1" />
-        <option value="Projeto 2" />
-        <option value="Projeto 3" />
-        <option value="Banana" />
+        {cycles.map((cycle) => (
+          <option key={cycle.id} value={cycle.task} />
+        ))}
       </datalist>
 
       <label htmlFor="">durante </label>
